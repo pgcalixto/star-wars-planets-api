f0fa41702df215473cd5b8657c79d4d2ef26c74d
Pedro Mendonça <pgcalixto@gmail.com>
2018-10-27 19:51:13 -0300

    Add PyMongo integration
    Now instead of using a Python list as global variable to store
    the planets, the app now stores and retrieves the planet from
    a mongo database.

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import json
+from bson import json_util
 from flask import Flask
+from flask_pymongo import PyMongo
 from flask_restful import reqparse, Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/starwars"
 
-planets = []
+api = Api(app)
+mongo = PyMongo(app)
+planets_col = mongo.db["planets"]
 
 # TODO Request parsing will be discontinued from flask_restful. Change it to
 # another library which implements request parsing (e.g.: Marshmallow).
@@ -15,12 +20,18 @@ parser.add_argument('terrain', required=True)
 
 class Planets(Resource):
     def get(self):
-        return planets
+        planets = planets_col.find()
+        return json.loads(json_util.dumps(planets))
 
     def put(self):
+        # insert planet
         planet = parser.parse_args()
-        planets.append(planet)
-        return planets
+        planet_id = planets_col.insert_one(planet).inserted_id
+        print(planet_id)
+
+        # return list of planets
+        planets = planets_col.find()
+        return json.loads(json_util.dumps(planets))
 
 api.add_resource(Planets, '/')
 
