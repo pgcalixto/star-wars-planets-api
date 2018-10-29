ee27d03a527d433808c0e7ec0f26331d1e52384c
Pedro Calixto <pgcalixto@gmail.com>
2018-10-29 20:38:30 -0300

    Move removal of all planets to a fixture
    The removal of all planets was already being tested in another
    test case. It made more sense for the removal of all planets
    to be a precondition for the initial tests of the API, so now
    it is a fixture.

--- test_flaskr.py
+++ test_flaskr.py
@@ -9,6 +9,18 @@ planet = {
 }
 
 @pytest.fixture
+def reset_client():
+    app.config['TESTING'] = True
+    test_app = app.test_client()
+
+    # get all planets and delete all
+    response = test_app.get('/planets')
+    for planet in response.json:
+        test_app.delete('/planets/' + planet['_id']['$oid'])
+
+    yield test_app
+
+@pytest.fixture
 def client():
     app.config['TESTING'] = True
     test_app = app.test_client()
@@ -16,22 +28,6 @@ def client():
 
 def test_get_planets_type(client):
     '''Asserts that GET /planets returns 200 and a JSON list.'''
-    response = client.get('/planets')
-    assert response.status_code == 200
-    assert type(response.json) == list
-
-def test_empty_all_planets(client):
-    '''
-    Asserts that all planets can be removed and GET /planets returns 200 and
-    an empty JSON list.
-    '''
-    # get all planets
-    response = client.get('/planets')
-
-    # delete all planets
-    for planet in response.json:
-        client.delete('/planets/' + planet['_id']['$oid'])
-
     response = client.get('/planets')
     assert response.status_code == 200
     assert response.json == []
