c849ee4b586578645f79b82eeb1c6625b67e21da
Pedro Calixto <pgcalixto@gmail.com>
2018-10-29 20:26:59 -0300

    Add test for emptying the planet list using DELETE

--- test_flaskr.py
+++ test_flaskr.py
@@ -12,3 +12,19 @@ def test_get_planets_type(client):
     response = client.get('/planets')
     assert response.status_code == 200
     assert type(response.json) == list
+
+def test_empty_all_planets(client):
+    '''
+    Asserts that all planets can be removed and GET /planets returns 200 and
+    an empty JSON list.
+    '''
+    # get all planets
+    response = client.get('/planets')
+
+    # delete all planets
+    for planet in response.json:
+        client.delete('/planets/' + planet['_id']['$oid'])
+
+    response = client.get('/planets')
+    assert response.status_code == 200
+    assert response.json == []
